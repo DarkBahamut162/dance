0867d765a75d9859ae25b9475b2799e72b4230f4
DarkBahamut162 <dark_bahamut162@hotmail.com>
2023-08-13 12:00:09 +0200

    Update (doubleres)

--- ITGCel/Down Receptor.lua
+++ ITGCel/Down Receptor.lua
@@ -1,7 +1,6 @@
 return Def.ActorFrame{
 	Def.Sprite{
 		Texture=NOTESKIN:GetPath('_down','Receptor Go'),
-		Frames=Sprite.LinearFrames(4,4),
 		InitCommand=function(self) self:effectclock("beat"):diffuseramp():effectcolor1(0.1,0.1,0.1,1):effectcolor2(1,1,1,1):effectperiod(0.5):effecttiming(0.25,0.50,0,0.25):effectoffset(-0.25) end,
 		NoneCommand=NOTESKIN:GetMetricA("ReceptorArrow","NoneCommand"),
 		PressCommand=NOTESKIN:GetMetricA("ReceptorArrow","PressCommand"),
@@ -14,13 +13,6 @@ return Def.ActorFrame{
 	},
 	Def.Sprite{
 		Texture=NOTESKIN:GetPath('_down','Receptor Go'),
-		Frames={
-			{Frame=0,Delay=0.25},
-			{Frame=1,Delay=1},
-			{Frame=2,Delay=1},
-			{Frame=3,Delay=1},
-			{Frame=0,Delay=0.75}
-		},
 		InitCommand=function(self) self:blend('BlendMode_Add'):diffusealpha(0) end,
 		NoneCommand=NOTESKIN:GetMetricA("ReceptorArrow","NoneCommand"),
 		PressCommand=function(self) self:diffusealpha(0.2) end,
